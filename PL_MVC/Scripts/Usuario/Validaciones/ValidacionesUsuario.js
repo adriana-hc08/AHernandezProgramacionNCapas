99d30ca9a275dfa934123860919134c444df90b1
Jesús Torres <jtorrstorrs@gmail.com>
2025-05-09 17:34:10 -0600

    clase Carga Masiva en PL

--- PL_MVC/Scripts/Usuario/Validaciones/ValidacionesUsuario.js
+++ PL_MVC/Scripts/Usuario/Validaciones/ValidacionesUsuario.js
@@ -167,6 +167,25 @@ document.getElementById("togglePasswordConfirm").addEventListener("click", funct
     this.classList.toggle("bi-eye-slash");
 });
 
+function validarForm() {
+    'use strict'
+
+    // Fetch all the forms we want to apply custom Bootstrap validation styles to
+    const forms = document.querySelectorAll('.needs-validation')
+
+    // Loop over them and prevent submission
+    Array.from(forms).forEach(form => {
+        form.addEventListener('submit', event => {
+            if (!form.checkValidity()) {
+                event.preventDefault()
+                event.stopPropagation()
+            }
+
+            form.classList.add('was-validated')
+        }, false)
+    })
+}
+document.addEventListener('DOMContentLoaded', validarForm);
 
 
 
